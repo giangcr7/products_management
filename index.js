f2f8bb2d40e142959bd1fb8bbc0b5d119007a0d5
giangcr7 <154339787+giangcr7@users.noreply.github.com>
2025-09-01 20:50:38 +0700

    ket noi csdl

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
 const route = require("./routes/client/index.route");
 
 const app = express();
 const port = process.env.PORT;
-
+mongoose.connect("mongodb://localhost:27017/product-management");
 app.set('views', './views');
 app.set('view engine', 'pug');
 
