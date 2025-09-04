8900e99f8d77742e9be9549c923bd1d16894c2f8
giangcr7 <154339787+giangcr7@users.noreply.github.com>
2025-09-04 13:01:10 +0700

    lay data ra giao dien

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,3 +1,18 @@
-module.exports.index = (req, res) => {
-    res.render('admin/pages/products/index.pug', { pageTitle: "Danh sách sản phẩm" });
+const Product = require("../../models/product.model");
+
+
+
+// GET /admin/products
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find(
+        {
+            deleted: false
+        }
+    );
+    console.log(products)
+    res.render('admin/pages/products/index.pug', {
+        pageTitle: "Danh sách sản phẩm",
+        products: products
+    });
 }
