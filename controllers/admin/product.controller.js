7f310d36fa964166c05d52642d6aca2c31285a22
giangcr7 <154339787+giangcr7@users.noreply.github.com>
2025-09-04 20:59:57 +0700

    toi uu tim kiem

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,8 @@
 const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
+const searchHelper = require("../../helpers/search");
+
 // GET /admin/products
 
 module.exports.index = async (req, res) => {
@@ -13,11 +15,11 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
 
     }
-    let keyword = "";
-    if (req.query.keyword) {
-        keyword = req.query.keyword;
-        const regex = new RegExp(keyword, "i");
-        find.title = regex;
+
+    const objectSearch = searchHelper(req.query);
+
+    if (objectSearch.regex) {
+        find.title = objectSearch.regex;
     }
 
     const products = await Product.find(find);
@@ -25,6 +27,6 @@ module.exports.index = async (req, res) => {
         pageTitle: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: keyword
+        keyword: objectSearch.keyword
     });
 }
