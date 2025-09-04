20f0bc52dab79650e23c1403d9df767677f67eea
giangcr7 <154339787+giangcr7@users.noreply.github.com>
2025-09-04 14:41:38 +0700

    tim kiem front end

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -19,3 +19,23 @@ if (buttonStatus.length > 0) {
 }
 
 // end xử lý button status
+
+// Xử lý tìm kiếm
+
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+    let url = new URL(window.location.href);
+    formSearch.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        if (keyword) {
+            url.searchParams.set("keyword", keyword);
+        }
+        else {
+            url.searchParams.delete("keyword");
+        }
+        window.location.href = url.href;
+    });
+}
+
+// end xử lý tìm kiếm
