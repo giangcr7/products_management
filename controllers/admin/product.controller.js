1de25b7c2e0d9a92a24f86cc1daccbd19f746594
giangcr7 <154339787+giangcr7@users.noreply.github.com>
2025-09-04 20:42:08 +0700

    toi uu phan bo loc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,43 +1,14 @@
 const Product = require("../../models/product.model");
 
-
-
+const filterStatusHelper = require("../../helpers/filterStatus");
 // GET /admin/products
 
 module.exports.index = async (req, res) => {
 
-    let filterStatus =
-        [
-            {
-                name: "Tất cả",
-                status: "",
-                class: ""
-            },
-            {
-                name: "Hoạt động",
-                status: "active",
-                class: ""
-            },
-            {
-                name: "Dừng hoạt động",
-                status: "inactive",
-                class: ""
-            }
-        ]
+    const filterStatus = filterStatusHelper(req.query);
     let find = {
         deleted: false
     };
-
-    if (req.query.status) {
-        const index = filterStatus.findIndex(item => item.status == req.query.status);
-        filterStatus[index].class = "active";
-    }
-    else {
-        const index = filterStatus.findIndex(item => item.status == "");
-        filterStatus[index].class = "active";
-
-
-    }
     if (req.query.status) {
         find.status = req.query.status;
 
@@ -50,9 +21,6 @@ module.exports.index = async (req, res) => {
     }
 
     const products = await Product.find(find);
-    // console.log(products)
-
-
     res.render('admin/pages/products/index.pug', {
         pageTitle: "Danh sách sản phẩm",
         products: products,
