83d8b979b3f50bd75d1b7327513d85942b939d10
giangcr7 <154339787+giangcr7@users.noreply.github.com>
2025-09-12 16:42:48 +0700

    chinh sua san pham

--- validates/admin/product.validate.js
+++ validates/admin/product.validate.js
@@ -1,4 +1,4 @@
-module.exports.createPost = (req, res) => {
+module.exports.createPost = (req, res, next) => {
     if (!req.body.title) {
         req.flash("error", `Vui lòng nhập tiêu đề`);
         res.redirect(req.get("Referer") || "/admin/products");
